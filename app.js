cf8b050dc8931efa58d75671de2dc0edec8a1bd8
Puspalal <puspalalnewar79@gmail.com>
2024-06-19 11:49:59 +0530

    Fixed the problem of local storage

--- app.js
+++ app.js
@@ -8,58 +8,70 @@ const listItems = document.querySelectorAll(".list-item");
 
 
 
-if(item.value.length === 0){
+
+if (item.value.length === 0) {
     btnAdd.disabled = true;
     btnAdd.style.cursor = "no-drop";
 }
-item.addEventListener("keyup", ()=>{
+item.addEventListener("keyup", () => {
     btnAdd.disabled = false;
     btnAdd.style.cursor = "pointer";
 });
 
 // let count = 0;
 
-btnAdd.addEventListener("click", ()=>{
-    total.innerText =  parseInt(total.innerText) + parseInt(amount.value) + "Rs";
+btnAdd.addEventListener("click", () => {
+    
+    total.innerText = parseInt(total.innerText) + parseInt(amount.value) + "Rs";
     let newRec = document.createElement("p");
     newRec.classList.add("list-item");
     newRec.innerText = `${item.value} : ${amount.value} Rs`;
     // let value = `${item.value} : ${amount.value} Rs`;
     const requestBody = {
-        name : item.value,
-        amount : amount.value,
-        rupees : total.innerText,
+        name: item.value,
+        amount: amount.value,
+        rupees: total.innerText,
     };
-    const storeData = JSON.parse(localStorage.getItem("items")) ||  [];
+    const storeData = JSON.parse(localStorage.getItem("items")) || [];
     storeData.push(requestBody);
     // console.log(requestBody);
 
     localStorage.setItem("items", JSON.stringify(storeData));
-    
+
     container.appendChild(newRec);
+    
     item.value = "";
     amount.value = "";
     btnAdd.disabled = true;
     btnAdd.style.cursor = "no-drop";
+    // listItems.style.display = "block";
 
 });
+
+btnClear.addEventListener("click", () => {
+    // listItems.style.display = "none";
+    localStorage.clear();
+    total.innerText = "00";
+    location.reload();
+});
+
 const fetchData = JSON.parse(localStorage.getItem("items"));
 console.log(fetchData);
 
-fetchData.forEach((e)=>{
-    total.innerText =  `${e.rupees}`;
-    let newRec = document.createElement("p");
-    newRec.classList.add("list-item");
-    newRec.innerText = `${e.name} : ${e.amount} Rs`;
-    container.appendChild(newRec);
-});
+if(fetchData === null){
+    // total.innerText = "120";
+}else{
+    fetchData.forEach((e) => {
 
-btnClear.addEventListener("click", ()=>{
-    localStorage.clear();
-    
-    // location.reload();
-    total.innerText = "00";  
-    window.reload();
-})
+        total.innerText = `${e.rupees}`;
+        let newRec = document.createElement("p");
+        newRec.classList.add("list-item");
+        newRec.innerText = `${e.name} : ${e.amount} Rs`;
+        container.appendChild(newRec);
+    });
+}
+
+
+
 
 
