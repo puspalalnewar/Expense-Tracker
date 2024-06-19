d5c7274a746a5081177de005f174455fffb160b8
Puspalal <puspalalnewar79@gmail.com>
2024-06-19 12:13:32 +0530

    Worked on responsive

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const container = document.querySelector(".container");
 const total = document.getElementById("total");
 const listItems = document.querySelectorAll(".list-item");
 
-
-
-
 if (item.value.length === 0) {
     btnAdd.disabled = true;
     btnAdd.style.cursor = "no-drop";
@@ -18,15 +15,12 @@ item.addEventListener("keyup", () => {
     btnAdd.style.cursor = "pointer";
 });
 
-// let count = 0;
-
 btnAdd.addEventListener("click", () => {
-    
+
     total.innerText = parseInt(total.innerText) + parseInt(amount.value) + "Rs";
     let newRec = document.createElement("p");
     newRec.classList.add("list-item");
     newRec.innerText = `${item.value} : ${amount.value} Rs`;
-    // let value = `${item.value} : ${amount.value} Rs`;
     const requestBody = {
         name: item.value,
         amount: amount.value,
@@ -34,22 +28,15 @@ btnAdd.addEventListener("click", () => {
     };
     const storeData = JSON.parse(localStorage.getItem("items")) || [];
     storeData.push(requestBody);
-    // console.log(requestBody);
-
     localStorage.setItem("items", JSON.stringify(storeData));
-
     container.appendChild(newRec);
-    
     item.value = "";
     amount.value = "";
     btnAdd.disabled = true;
     btnAdd.style.cursor = "no-drop";
-    // listItems.style.display = "block";
-
 });
 
 btnClear.addEventListener("click", () => {
-    // listItems.style.display = "none";
     localStorage.clear();
     total.innerText = "00";
     location.reload();
@@ -58,9 +45,9 @@ btnClear.addEventListener("click", () => {
 const fetchData = JSON.parse(localStorage.getItem("items"));
 console.log(fetchData);
 
-if(fetchData === null){
-    // total.innerText = "120";
-}else{
+if (fetchData === null) {
+
+} else {
     fetchData.forEach((e) => {
 
         total.innerText = `${e.rupees}`;
